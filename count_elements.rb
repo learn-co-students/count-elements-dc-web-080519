3082e9d6ed38e6980be619dffdf302c0714857f5
github username <github email address>
2019-08-06 08:47:55 -0400

    Completed Element Count Lab

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+  count = Hash.new(0)
+  # creates a new hash (count) with a value of zero?
+    array.each {|item| count[item] += 1}
+    # takes existing array and iterates through it
+    # |item| sets the "item" in the hash as the key of the hash?
+    # count[item] += 1 makes the value of the hash and increments
+    # up if there are multiple animals
+  count
+  # returns count hash
 end
  
