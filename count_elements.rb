039e240f7ebcd0a151d488376e067de1b1005cd5
Pete Hanner <petehanner@gmail.com>
2019-08-05 21:23:56 -0400

    Counting inside an array

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,17 @@
 def count_elements(array)
-  # code goes here
+  # initialize return value
+  new_hash = {}
+
+  # loop over each value in argument
+  array.each do |animal|
+    # create key-value at count 1 if key not present
+    if !(new_hash[animal])
+      new_hash[animal] = 1
+    # increment value if key present
+    else
+      new_hash[animal] += 1
+    end
+  end
+  # return
+  new_hash
 end
- 
